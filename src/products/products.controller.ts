c5abdf831ad1b263ec5d44a94499ac01d7f2c640
Ma-Brun <cyrus_brun27@milton.edu>
2026-05-11 21:09:58 -0400

    finished

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, Put, Delete } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
     constructor(private readonly productsService: ProductsService) {}
-    @Post()
-    addProduct(@Body() prodTitle: string, @Body() prodDescription: string, @Body() prodPrice: number): any {
-        const idOfProd = this.productsService.newProduct(prodTitle, prodDescription, prodPrice);
-        return {id: idOfProd };
+    @Post('add')
+    addProduct(
+        @Body('title') title: string,
+        @Body('description') description: string,
+        @Body('price') price: number,
+    )
+    {
+    const idOfProd = this.productsService.newProduct(title, description, price);
+    return {id: idOfProd };
     }
+    @Get()
+    getAllProducts() {
+        return this.productsService.getAllProducts();
+    }
+    @Get(":name")
+    getProduct(@Param('name') name: string) {
+        return this.productsService.getSingleProduct(name);
+    }
+    @Patch(":name")
+    patchProduct(@Param('name') name: string, @Body('description') description: string, @Body('price') price: number) {
+        this.productsService.patchProduct(name, description, price);
+        return this.productsService.getSingleProduct(name);
+    }
+    @Delete(":name")
+    deleteProduct(@Param('name') name: string) {
+        this.productsService.deleteProduct(name);
+        return { message: 'Product deleted successfully' };
+    }
+
 }
 
 
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,17 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
 @Injectable()
 export class ProductsService {
-    products: Product[] = [];
-
-    addProduct(prod : Product) {
-        this.products.push(prod);
-    }
+    private products: Product[] = [];
 
     newProduct(name: string, description: string, price: number) {
         const id = new Date().getTime();
         const newProd = new Product(id, name, description, price);
-        this.addProduct(newProd);
+        this.products.push(newProd);
         return id;
+    }
+    getAllProducts() {
+        return [...this.products];
+    }
+
+    getSingleProduct(name: string) {
+        if (!this.products.find((prod) => prod.name === name)) {
+            throw new NotFoundException("Product not found");
+        }
+        else {
+        return {...this.products.find((prod) => prod.name === name)};}
+    }
+    patchProduct(name: string, description: string, price: number) {
+        const productIndex = this.products.findIndex((prod) => prod.name === name);
+
+        if (productIndex === -1) {
+            throw new NotFoundException("Product not found");
+        }
+        else {
+            if (description !== undefined) {                    
+                this.products[productIndex].description = description;
+            }
+            if (price !== undefined) {
+                this.products[productIndex].price = price;
+            }
+            if (description === undefined && price === undefined) {
+                throw new NotFoundException("No fields to update");
+            }
+            return {...this.products[productIndex]};
+        }
+    }
+
+    deleteProduct(name: string) {
+        const productIndex = this.products.findIndex((prod) => prod.name === name);
+        if (productIndex === -1) {
+            throw new NotFoundException("Product not found");
+        }
+        this.products.splice(productIndex, 1);
     }
 }
