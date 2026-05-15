a28147f1c3001af25d27f41d5938d4699e22c56f
Ma-Brun <cyrus_brun27@milton.edu>
2026-05-15 11:42:06 -0400

    new stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { MongooseModule} from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot("mongodb+srv://new-user:4ip72vondyDBCy2c@cluster0.s0upaf7.mongodb.net/")],
   controllers: [AppController],
   providers: [AppService],
 })
